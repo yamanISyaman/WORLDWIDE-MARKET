49ed28ae1c9c734819f91b0938f6e4c79ad71709
yamanoML <yamanoml@noreply.codeberg.org>
2023-02-11 16:47:21 +0000

    Upload files to 'bot'

--- bot/handy.py
+++ bot/handy.py
@@ -2,10 +2,11 @@ import sqlite3
 import telebot
 import responses
 
-def sql(query, vars = None):
-    ''' executing the sql query with handling making a connection and closing it '''
+
+def sql(query, vars=None):
+    """executing the sql query with handling making a connection and closing it"""
     # connecting to the database
-    connection = sqlite3.connect('data.db')
+    connection = sqlite3.connect("data.db")
     # cursor
     cur = connection.cursor()
     # executing
@@ -20,42 +21,50 @@ def sql(query, vars = None):
 
 
 def get_fullname(message):
-    '''takes a message and return user's full name'''
+    """takes a message and return user's full name"""
     first_name = message.chat.first_name
     last_name = message.chat.last_name
     if last_name == None:
         fullname = first_name
     else:
-        fullname = first_name + ' '+ last_name 
+        fullname = first_name + " " + last_name
     return fullname
 
 
 def check_for_newuser(message):
-    ''' user is new then add them to the list'''
+    """user is new then add them to the list"""
     users_list = sql("SELECT id FROM users")
     users = []
     for i in users_list:
         users.append(i[0])
-    if message.chat.id in users :
+    if message.chat.id in users:
         return False
     else:
-        sql("INSERT INTO users(id, name) VALUES (?, ?)", (message.chat.id, get_fullname(message)))
+        sql(
+            "INSERT INTO users(id, name) VALUES (?, ?)",
+            (message.chat.id, get_fullname(message)),
+        )
         return True
 
 
 def cancelling(message):
-    ''' handling the cleaning and directing when the use cancel the adding-product process'''
-    
+    """handling the cleaning and directing when the use cancel the adding-product process"""
+
     markup = telebot.types.ReplyKeyboardRemove()
-    responses.bot.send_message(chat_id=message.chat.id, text="--Process Is Cancelled--" , reply_markup=markup)
+    responses.bot.send_message(
+        chat_id=message.chat.id, text="--Process Is Cancelled--", reply_markup=markup
+    )
     responses.buttons_input(message.chat.id, "/start")
-        # deleting the cancelled data
-    sql("DELETE FROM products WHERE owner_id = ? AND photo_id IS NULL",(message.chat.id,))
+    # deleting the cancelled data
+    sql(
+        "DELETE FROM products WHERE owner_id = ? AND photo_id IS NULL",
+        (message.chat.id,),
+    )
     return
 
 
 def is_admin(id):
-    '''check if the user is admin or not'''
+    """check if the user is admin or not"""
     admins = sql("SELECT id FROM users WHERE is_admin > 0")
     user = (id,)
     if user in admins:
@@ -63,8 +72,9 @@ def is_admin(id):
     else:
         return False
 
+
 def is_owner(id):
-    '''check if the user is the owner or not'''
+    """check if the user is the owner or not"""
     admins = sql("SELECT id FROM users WHERE is_admin = 2")
     user = (id,)
     if user in admins:
@@ -80,15 +90,17 @@ def is_user(id):
     else:
         return False
 
+
 def no_owner():
-    owners =sql("SELECT id FROM users WHERE is_admin = 2")
+    owners = sql("SELECT id FROM users WHERE is_admin = 2")
     if len(owners) == 0:
         return True
     else:
         return False
 
+
 def make_inlines(*args):
-    '''this function takes a tuples that contains button_text and callback data and returns '''
+    """this function takes a tuples that contains button_text and callback data and returns"""
     markup = telebot.types.InlineKeyboardMarkup(row_width=2)
     buttons = []
     for i in args:
@@ -96,15 +108,15 @@ def make_inlines(*args):
     counter = 0
     length = len(buttons)
     while counter + 2 <= length:
-        markup.row(*buttons[counter:counter+ 2])
+        markup.row(*buttons[counter : counter + 2])
         counter += 2
     if length % 2 != 0:
         markup.add(buttons[-1])
     return markup
 
 
-def make_post(data : tuple):
-    '''every tuple should have (id, name, price, description, country, city, photo_id'''
+def make_post(data: tuple):
+    """every tuple should have (id, name, price, description, country, city, photo_id"""
     id = data[0]
     name = data[1]
     price = str(data[2])
@@ -114,13 +126,18 @@ def make_post(data : tuple):
     photo_id = data[6]
 
     text = f"{name}\n\n{price} $USD\n\n{description}\n\n{country} || {city}"
-    entities = [telebot.types.MessageEntity("bold",0, len(name) + len(price) + 6), telebot.types.MessageEntity("italic" ,text.find(country), len(country) + len(city))]
+    entities = [
+        telebot.types.MessageEntity("bold", 0, len(name) + len(price) + 6),
+        telebot.types.MessageEntity(
+            "italic", text.find(country), len(country) + len(city)
+        ),
+    ]
 
     return (text, entities, photo_id, id)
-        
+
 
 def tell_admins(text=None, entities=None):
-    ''' sends text to admins and returns a list of admins IDs '''
+    """sends text to admins and returns a list of admins IDs"""
     admins_sql = sql("SELECT id FROM users WHERE is_admin > 0")
     admins = []
     for admin in admins_sql:
@@ -129,5 +146,7 @@ def tell_admins(text=None, entities=None):
             if entities == None:
                 responses.bot.send_message(chat_id=admin[0], text=text)
             else:
-                responses.bot.send_message(chat_id=admin[0], text=text, entities=entities)
+                responses.bot.send_message(
+                    chat_id=admin[0], text=text, entities=entities
+                )
     return admins
--- bot/main.py
+++ bot/main.py
@@ -4,13 +4,11 @@ from responses import *
 from handy import *
 from welcoming import w_text
 
-    
-
 
 # handling any input
-@bot.message_handler(content_types=['photo', 'text'])
+@bot.message_handler(content_types=["photo", "text"])
 def action(message):
-    '''making an appropriate response for every message'''
+    """making an appropriate response for every message"""
 
     if no_owner():
         no_owner_action(message)
@@ -18,19 +16,22 @@ def action(message):
     elif check_for_newuser(message):
         user_name = get_fullname(message)
         buttons_input(message.chat.id, "/start")
-        bot.send_message(chat_id=message.chat.id, text=f"Hi, {user_name}\n THANKS FOR STARTING THE BOT\n\n{w_text}")
+        bot.send_message(
+            chat_id=message.chat.id,
+            text=f"Hi, {user_name}\n THANKS FOR STARTING THE BOT\n\n{w_text}",
+        )
         tell_admins(f"{user_name} has just started the bot!")
     # display buttons when it must to
     elif message.text == "/start":
         buttons_input(message.chat.id, message.text)
 
     # do the process of adding a product when the user asks to
-    elif message.text == 'Add Product':
+    elif message.text == "Add Product":
         buttons_input(message.chat.id, message.text)
         add_product(message)
 
     # showing posts when adimn asks to
-    elif message.text == "Pending Posts" and is_admin(message.chat.id): 
+    elif message.text == "Pending Posts" and is_admin(message.chat.id):
         pending_posts(message)
 
     elif message.text == "Find Product":
@@ -59,15 +60,16 @@ def action(message):
 
     elif message.text == "Add Admin" and is_owner(message.chat.id):
         add_admin(message)
-    
+
     elif message.text == "Remove Admin" and is_owner(message.chat.id):
         remove_admin(message)
 
     elif message.text == "Show Admins" and is_owner(message.chat.id):
         show_admins(message)
-        
+
+
 # handling callback queries
-@bot.callback_query_handler(func=lambda call:True)
+@bot.callback_query_handler(func=lambda call: True)
 def call_action(call):
     answercall(call)
 
@@ -75,24 +77,36 @@ def call_action(call):
 # handling the shipping query
 @bot.shipping_query_handler(func=lambda query: True)
 def shipping(shipping_query):
-
     # making shipping options
-    shipping_options = [telebot.types.ShippingOption(id='pickup', title='Local Pickup').add_price(telebot.types.LabeledPrice('Pickup', 0)), telebot.types.ShippingOption(id='delivery', title='Delivery').add_price(telebot.types.LabeledPrice('delivery', 500))]
+    shipping_options = [
+        telebot.types.ShippingOption(id="pickup", title="Local Pickup").add_price(
+            telebot.types.LabeledPrice("Pickup", 0)
+        ),
+        telebot.types.ShippingOption(id="delivery", title="Delivery").add_price(
+            telebot.types.LabeledPrice("delivery", 500)
+        ),
+    ]
 
-    #print(shipping_query)
-    bot.answer_shipping_query(shipping_query.id, ok=True, shipping_options=shipping_options)
+    # print(shipping_query)
+    bot.answer_shipping_query(
+        shipping_query.id, ok=True, shipping_options=shipping_options
+    )
+
 
 # handling pre check out query
 @bot.pre_checkout_query_handler(func=lambda query: True)
 def checkout(pre_checkout_query):
-    #print(pre_checkout_query)
-    bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True, error_message="SEEMS LIKE YOU'RE TRYING TO BUY FROM AN EXPIRED INVOICE\n\nPLEASE START A NEW INVOICE AND USE IT INSTEAD")
+    # print(pre_checkout_query)
+    bot.answer_pre_checkout_query(
+        pre_checkout_query.id,
+        ok=True,
+        error_message="SEEMS LIKE YOU'RE TRYING TO BUY FROM AN EXPIRED INVOICE\n\nPLEASE START A NEW INVOICE AND USE IT INSTEAD",
+    )
 
 
 # handling successful payment
-@bot.message_handler(content_types=['successful_payment'])
+@bot.message_handler(content_types=["successful_payment"])
 def got_payment(message):
-    
     payment_action(message)
 
 
